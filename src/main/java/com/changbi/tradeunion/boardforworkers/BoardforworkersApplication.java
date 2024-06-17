d25c045a21381115505ab0bb18d4d0b8dd9b3259
jongwan <jw.kim@changbi.com>
2024-06-17 10:25:51 +0900

    feat: first init

---
+++ src/main/java/com/changbi/tradeunion/boardforworkers/BoardforworkersApplication.java
@@ -0,0 +1,13 @@
+package com.changbi.tradeunion.boardforworkers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardforworkersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardforworkersApplication.class, args);
+    }
+
+}
